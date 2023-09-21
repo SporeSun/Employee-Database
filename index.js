619e6de059f2c6a6825a0e22b9bfd64458c9e796
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-09-20 19:10:49 -0700

    added code to index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const db = require('./queries.js');
+
+const start = async () => {
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            // ... Add other choices
+            'Exit'
+        ]
+    });
+
+    switch (answer.action) {
+        case 'View all departments':
+            const departments = await db.viewAllDepartments();
+            console.table(departments[0]);
+            start();
+            break;
+        // ... Add other cases
+
+        case 'Exit':
+            process.exit(0);
+            break;
+    }
+};
+
+start();
